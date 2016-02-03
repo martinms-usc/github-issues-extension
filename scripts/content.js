63083af7771daa919ce8682dc78fe13b694a7b12
Mike Martin <martinms.usc@gmail.com>
2016-02-03 12:18:00 -0800

    convert indentation to spaces

--- scripts/content.js
+++ scripts/content.js
@@ -3,14 +3,14 @@ var time = d.toLocaleTimeString();
 
 // pull some info from the issue page
 var issueInfo = {
-	name: document.getElementsByClassName('js-issue-title')[0].textContent,
-	url: window.location.href,
-	time: time
+  name: document.getElementsByClassName('js-issue-title')[0].textContent,
+  url: window.location.href,
+  time: time
 };
 
 // send issue info back to background script
 chrome.runtime.sendMessage({
-		from: 'content',
-		subject: 'addNewIssue',
-		issue: issueInfo
+  from: 'content',
+  subject: 'addNewIssue',
+  issue: issueInfo
 });
